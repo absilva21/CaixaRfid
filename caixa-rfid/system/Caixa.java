ead29e67676ca54052f82380019c55f3626e042f
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2023-08-15 21:53:08 -0300

    Ambiente preparado

--- caixa-rfid/system/Caixa.java
+++ caixa-rfid/system/Caixa.java
@@ -1,10 +1,16 @@
 package system;
 
-public class Caixa {
+import com.fazecast.jSerialComm.*;
 
+public class Caixa {
+	static SerialPort[] ports; 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("olá mundo");
+		ports = SerialPort.getCommPorts();
+		for(int i=0;i<ports.length;i++) {
+			System.out.println(ports[i].getDescriptivePortName());
+		}
 	}
 
 }
