de9f9140a470a96d80ad7814c57b9ffc0c0273f2
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2023-08-17 08:40:36 -0300

    Criando classes

--- caixa-rfid/system/Caixa.java
+++ caixa-rfid/system/Caixa.java
@@ -2,14 +2,18 @@ package system;
 
 import com.fazecast.jSerialComm.*;
 
+import java.io.IOException;
+import java.net.*;
+
 public class Caixa {
 	static SerialPort[] ports; 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		System.out.println("olá mundo");
+		
+		System.out.println("Olá, selecione a porta serial do leitor:\n");
 		ports = SerialPort.getCommPorts();
 		for(int i=0;i<ports.length;i++) {
-			System.out.println(ports[i].getDescriptivePortName());
+			System.out.println(ports[i].getDescriptivePortName()+"\n");
 		}
 	}
 
