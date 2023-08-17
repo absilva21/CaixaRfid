8bbb9184ca3d6f07af5bc44e9ac8f4a648228bcb
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2023-08-17 10:11:55 -0300

    Ajustes

--- Caixa/src/application/Main.java
+++ Caixa/src/application/Main.java
@@ -1,18 +1,19 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			AnchorPane root = FXMLLoader.load(getClass().getResource("application.fxml"));
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
 			primaryStage.setFullScreen(true);
 			primaryStage.show();
