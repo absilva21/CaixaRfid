eb150232265b38847810cc465e66844440c6ea3f
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2023-08-30 17:49:22 -0300

    consulta de pproduto pelo caixa

--- Caixa/src/application/Caixa.java
+++ Caixa/src/application/Caixa.java
@@ -5,11 +5,22 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import java.net.Socket;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Scanner;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
 
 
 
@@ -32,39 +43,76 @@ public class Caixa extends Application {
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		//launch(args);
-		Socket leitor = new Socket("192.168.0.111",70);
 		
+		Scanner teclado = new Scanner(System.in);
+		String comando = "";
 		
+		while(true) {
+			System.out.println("Digite um comando:\n");
+			comando = teclado.next();
 			
-		BufferedWriter bufferOut = new BufferedWriter(new OutputStreamWriter(leitor.getOutputStream()));
-		bufferOut.write( "read");
-		bufferOut.flush();
-		
-		BufferedReader bufferIn = new BufferedReader(new InputStreamReader(leitor.getInputStream()));
+			if(comando.equals("l")) {
+				Socket leitor = new Socket("localhost",70);
+				
+				BufferedWriter bufferOut = new BufferedWriter(new OutputStreamWriter(leitor.getOutputStream()));
+				bufferOut.write( "read");
+				bufferOut.flush();
+				
+				BufferedReader bufferIn = new BufferedReader(new InputStreamReader(leitor.getInputStream()));
+				
+			
+				
+				String req = "";
+				
+				char[] buf = new char[8388608];
+				bufferIn.read(buf);
+				
+				int n = 0;
+				
+				while(buf[n]!=0) {
+					req += buf[n];
+					n++;
+				}
+				
+			
+				bufferOut.close();
+				bufferIn.close();
+				leitor.close();
+				
+				String[] produtos = req.split("\n");
+				System.out.println("\nprodutos: \n");
+				
+				for(int i=0;i<produtos.length;i++) {
+					HttpClient client = HttpClient.newHttpClient();
+					HttpRequest request = HttpRequest.newBuilder()
+					          .GET()
+					          .timeout(Duration.ofSeconds(10))
+					          .uri(URI.create("http://localhost/produto?produto="+produtos[i]))
+					          .build();
+
+					HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+					if(response.statusCode()==200) {
+						JSONParser parser = new JSONParser();  
+						try {
+							JSONObject json = (JSONObject) parser.parse(response.body());
+							System.out.println("\ncodigo             descricão            valor\n");
+							System.out.println("\n"+json.get("codigo")+"             "+json.get("descricao")+"           R$"+ json.get("valor")+"\n");
+						}catch(ParseException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				
+			}
+			
+			if(comando.equals("x")) {
+				System.exit(0);
+			}
+			
+			
+		}
 		
 	
-		
-		String req = "";
-		
-		char[] buf = new char[8388608];
-		bufferIn.read(buf);
-		
-		int n = 0;
-		
-		while(buf[n]!=0) {
-			req += buf[n];
-			n++;
-		}
-		bufferOut.close();
-		bufferIn.close();
-		
-		leitor.close();
-		System.out.println("produtos:\n");
-		System.out.println(req);
-		
-
-		
-		System.exit(0);
 
 	}
 }
